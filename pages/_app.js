802a0728f120cd3278059eeb6bc338d69cb593a9
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-12 21:04:20 +0100

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles/global.css"
+import "../styles/dashboard.css"
+import "../styles/admin.css"
+
+export default function App({ Component, pageProps }) {
+return <Component {...pageProps} />
+}
