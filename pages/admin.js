e55002520f955c07e33086d225952ed10399fd3c
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-12 16:11:00 +0100

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -6,7 +6,7 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
-export default function Admin() {
+export default function AdminBeheer() {
   const [users, setUsers] = useState([])
   const [loading, setLoading] = useState(true)
 
