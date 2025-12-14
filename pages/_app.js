9167ffc7b56377b041a5777f6f806e969f0f0954
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-14 17:18:44 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,34 @@
-import "../styles/global.css"
-import "../styles/dashboard.css"
-import "../styles/admin.css"
-import Layout from "../components/Layout"
+import "../styles/globals.css"
 
-export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
+export default function MyApp({ Component, pageProps }) {
+return (
+<div className="sb-app">
+<aside className="sb-sidebar">
+<div className="sb-logo">SterkBouw</div>
+<nav className="sb-nav">
+<a href="/dashboard">Dashboard</a>
+<a href="/projecten">Projecten</a>
+<a href="/calculator">Calculaties</a>
+<a href="/stabu-calculator">STABU</a>
+<a href="/bim">BIM</a>
+<a href="/constructeurs">Constructeurs</a>
+<a href="/risicoanalyse">Risico</a>
+<a href="/kopersportaal">Kopersportaal</a>
+</nav>
+</aside>
+
+  <main className="sb-main">
+    <header className="sb-header">
+      <div>Dashboard</div>
+      <div>Ingelogd</div>
+    </header>
+
+    <section className="sb-content">
       <Component {...pageProps} />
-    </Layout>
-  )
+    </section>
+  </main>
+</div>
+
+
+)
 }
