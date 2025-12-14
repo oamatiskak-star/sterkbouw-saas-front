53b5f66456572cbc1d4554c69eceae1780d1efcd
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-14 17:33:11 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,31 @@
 import "../styles/global.css"
+import { modules } from "../lib/modules"
 
 export default function MyApp({ Component, pageProps }) {
-return (
-<div className="sb-app">
-<aside className="sb-sidebar">
-<div className="sb-logo">SterkBouw</div>
-<nav className="sb-nav">
-<a href="/dashboard">Dashboard</a>
-<a href="/projecten">Projecten</a>
-<a href="/calculator">Calculaties</a>
-<a href="/stabu-calculator">STABU</a>
-<a href="/bim">BIM</a>
-<a href="/constructeurs">Constructeurs</a>
-<a href="/risicoanalyse">Risico</a>
-<a href="/kopersportaal">Kopersportaal</a>
-</nav>
-</aside>
+  return (
+    <div className="sb-app">
+      <aside className="sb-sidebar">
+        <div className="sb-logo">SterkBouw</div>
 
-  <main className="sb-main">
-    <header className="sb-header">
-      <div>Dashboard</div>
-      <div>Ingelogd</div>
-    </header>
+        <nav className="sb-nav">
+          {modules.map(m => (
+            <a key={m.slug} href={`/${m.slug}`}>
+              {m.title}
+            </a>
+          ))}
+        </nav>
+      </aside>
 
-    <section className="sb-content">
-      <Component {...pageProps} />
-    </section>
-  </main>
-</div>
+      <main className="sb-main">
+        <header className="sb-header">
+          <div>Dashboard</div>
+          <div>Ingelogd</div>
+        </header>
 
-
-)
+        <section className="sb-content">
+          <Component {...pageProps} />
+        </section>
+      </main>
+    </div>
+  )
 }
