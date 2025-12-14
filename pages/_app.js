6c207fd623be32c1210527ebb45206032c89c6ba
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-14 15:19:54 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,12 @@ import "../styles/global.css"
 import "../styles/dashboard.css"
 import "../styles/admin.css"
 
+import DashboardLayout from "../components/DashboardLayout"
+
 export default function App({ Component, pageProps }) {
-return <Component {...pageProps} />
+return (
+<DashboardLayout>
+<Component {...pageProps} />
+</DashboardLayout>
+)
 }
