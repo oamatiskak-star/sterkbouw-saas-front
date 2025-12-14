4e4f81d88760d72bd143500eed90a15fbde76041
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-14 18:07:21 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,20 +6,33 @@ export default function MyApp({ Component, pageProps }) {
     <div className="sb-app">
       <aside className="sb-sidebar">
         <div className="sb-logo">SterkBouw</div>
+
         <nav className="sb-nav">
           {menuConfig.map(m => (
             <div key={m.key} className="sb-group">
               <div className="sb-group-title">{m.title}</div>
+
               {m.actions.map(a => (
-                <a key={a.id} href={`/actie/${a.id}`}>{a.label}</a>
+                <a
+                  key={a.id}
+                  href={`/workspace?action=${a.id}`}
+                >
+                  {a.label}
+                </a>
               ))}
             </div>
           ))}
         </nav>
       </aside>
+
       <main className="sb-main">
-        <header className="sb-header"><div>Dashboard</div></header>
-        <section className="sb-content"><Component {...pageProps} /></section>
+        <header className="sb-header">
+          <div>Workspace</div>
+        </header>
+
+        <section className="sb-content">
+          <Component {...pageProps} />
+        </section>
       </main>
     </div>
   )
