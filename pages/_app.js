87c89b1440c5b72b464a327e99f54074a4ac5fb8
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-14 17:56:58 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,25 @@
 import "../styles/global.css"
-import { modules } from "../lib/modules"
+import { menuConfig } from "../lib/menuConfig"
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className="sb-app">
       <aside className="sb-sidebar">
         <div className="sb-logo">SterkBouw</div>
-
         <nav className="sb-nav">
-          {modules.map(m => (
-            <a key={m.slug} href={`/${m.slug}`}>
-              {m.title}
-            </a>
+          {menuConfig.map(m => (
+            <div key={m.key} className="sb-group">
+              <div className="sb-group-title">{m.title}</div>
+              {m.actions.map(a => (
+                <a key={a.id} href={`/actie/${a.id}`}>{a.label}</a>
+              ))}
+            </div>
           ))}
         </nav>
       </aside>
-
       <main className="sb-main">
-        <header className="sb-header">
-          <div>Dashboard</div>
-          <div>Ingelogd</div>
-        </header>
-
-        <section className="sb-content">
-          <Component {...pageProps} />
-        </section>
+        <header className="sb-header"><div>Dashboard</div></header>
+        <section className="sb-content"><Component {...pageProps} /></section>
       </main>
     </div>
   )
