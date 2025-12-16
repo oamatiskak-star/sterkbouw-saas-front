7e882e30455bf6fbf2e8a4b3f26e2ff66c7d899a
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-16 14:04:29 +0100

    Update bouw.js

--- pages/calculaties/bouw.js
+++ pages/calculaties/bouw.js
@@ -1,10 +1,14 @@
-import ModulePage from "../../components/ModulePage"
-
-export default function CalculatiesBouw() {
+export default function CalculatieBouw() {
   return (
-    <ModulePage
-      modulePath="calculaties/bouw"
-      title="Calculaties – Bouw"
-    />
+    <div>
+      <h1>Calculatie Bouw</h1>
+
+      <div style={{ display: "grid", gap: 12, marginTop: 20 }}>
+        <button>Nieuwe calculatie</button>
+        <button>Bestand uploaden</button>
+        <button>Calculatie starten</button>
+        <button>Resultaat bekijken</button>
+      </div>
+    </div>
   )
 }
