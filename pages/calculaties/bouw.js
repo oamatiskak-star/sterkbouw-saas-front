bccbad19d1d68f3af86d1cdc8e65ef2f585a1d96
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-16 13:17:07 +0100

    Create bouw.js

---
+++ pages/calculaties/bouw.js
@@ -0,0 +1,10 @@
+import ModulePage from "../../components/ModulePage"
+
+export default function CalculatiesBouw() {
+  return (
+    <ModulePage
+      modulePath="calculaties/bouw"
+      title="Calculaties – Bouw"
+    />
+  )
+}
