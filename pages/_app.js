7ef71942466df503c26d2561d1c7851e9d6db005
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-16 15:55:32 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,9 @@
 import "../styles/global.css"
 import Layout from "../components/Layout"
-import { useRouter } from "next/router"
 
 export default function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  /*
-    Bepaal actieve sidebar sectie
-  */
-  const path = router.pathname
-
-  let active = ""
-
-  if (path.startsWith("/dashboard")) active = "dashboard"
-  else if (path.startsWith("/projecten")) active = "projecten"
-  else if (path.startsWith("/calculaties")) active = "calculaties"
-  else if (path.startsWith("/project-ontwikkeling")) active = "project-ontwikkeling"
-  else if (path.startsWith("/uploads")) active = "uploads"
-  else if (path.startsWith("/bim")) active = "bim"
-  else if (path.startsWith("/planning")) active = "planning"
-  else if (path.startsWith("/inkoop")) active = "inkoop"
-  else if (path.startsWith("/risico")) active = "risico"
-
   return (
-    <Layout active={active}>
+    <Layout>
       <Component {...pageProps} />
     </Layout>
   )
