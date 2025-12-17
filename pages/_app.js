d042499453f5d1627a5e229f741a5ef4f589fb82
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-17 15:26:08 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,40 +7,38 @@ import Sidebar from "../components/Sidebar"
 export const AppContext = createContext(null)
 
 export default function MyApp({ Component, pageProps }) {
-const [sidebarCollapsed, setSidebarCollapsed] = useState(false)
-  export default function MyApp({ Component, pageProps }) {
-return (
-<div style={{ display: "flex" }}>
-<Sidebar />
-<main style={{ flex: 1, padding: 24 }}>
-<Component {...pageProps} />
-</main>
-</div>
-)
-}
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false)
 
-const toggleSidebar = () => {
-setSidebarCollapsed(prev => {
-const next = !prev
-if (typeof window !== "undefined") {
-localStorage.setItem("sb-sidebar-collapsed", next ? "1" : "0")
-}
-return next
-})
-}
+  const toggleSidebar = () => {
+    setSidebarCollapsed(prev => {
+      const next = !prev
+      if (typeof window !== "undefined") {
+        localStorage.setItem(
+          "sb-sidebar-collapsed",
+          next ? "1" : "0"
+        )
+      }
+      return next
+    })
+  }
 
-const contextValue = {
-sidebarCollapsed,
-toggleSidebar,
-user: pageProps.user || null,
-role: pageProps.role || "admin"
-}
+  const contextValue = {
+    sidebarCollapsed,
+    toggleSidebar,
+    user: pageProps.user || null,
+    role: pageProps.role || "admin"
+  }
 
-return (
-<AppContext.Provider value={contextValue}>
-<TablerLayout>
-<Component {...pageProps} />
-</TablerLayout>
-</AppContext.Provider>
-)
+  return (
+    <AppContext.Provider value={contextValue}>
+      <div style={{ display: "flex", minHeight: "100vh" }}>
+        <Sidebar />
+        <main style={{ flex: 1 }}>
+          <TablerLayout>
+            <Component {...pageProps} />
+          </TablerLayout>
+        </main>
+      </div>
+    </AppContext.Provider>
+  )
 }
