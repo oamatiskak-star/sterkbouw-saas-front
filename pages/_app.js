d5b8fcbedc0f4051407317bfad88d511abdb2052
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-17 12:35:59 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,35 @@
 import "../styles/globals.css"
 import "@tabler/core/dist/css/tabler.min.css"
+import { useState, createContext } from "react"
 import TablerLayout from "../components/TablerLayout"
 
+export const AppContext = createContext(null)
+
 export default function MyApp({ Component, pageProps }) {
+const [sidebarCollapsed, setSidebarCollapsed] = useState(false)
+
+const toggleSidebar = () => {
+setSidebarCollapsed(prev => {
+const next = !prev
+if (typeof window !== "undefined") {
+localStorage.setItem("sb-sidebar-collapsed", next ? "1" : "0")
+}
+return next
+})
+}
+
+const contextValue = {
+sidebarCollapsed,
+toggleSidebar,
+user: pageProps.user || null,
+role: pageProps.role || "admin"
+}
+
 return (
+<AppContext.Provider value={contextValue}>
 <TablerLayout>
 <Component {...pageProps} />
 </TablerLayout>
+</AppContext.Provider>
 )
 }
