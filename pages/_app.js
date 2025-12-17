734c4d18f4ccb364eb954a214627744846c43285
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-17 11:12:19 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,8 @@
+import "@tabler/core/dist/css/tabler.min.css"
+import "@tabler/core/dist/css/tabler-flags.min.css"
+import "@tabler/core/dist/css/tabler-payments.min.css"
+import "@tabler/core/dist/css/tabler-vendors.min.css"
+
 import TablerLayout from "../components/TablerLayout"
 
 export default function App({ Component, pageProps }) {
