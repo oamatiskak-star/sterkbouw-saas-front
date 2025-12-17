efd3a187d5dd0305faabb2e3338b5394559935cb
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-17 15:25:09 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,22 @@ import "../styles/globals.css"
 import "@tabler/core/dist/css/tabler.min.css"
 import { useState, createContext } from "react"
 import TablerLayout from "../components/TablerLayout"
+import Sidebar from "../components/Sidebar"
 
 export const AppContext = createContext(null)
 
 export default function MyApp({ Component, pageProps }) {
 const [sidebarCollapsed, setSidebarCollapsed] = useState(false)
+  export default function MyApp({ Component, pageProps }) {
+return (
+<div style={{ display: "flex" }}>
+<Sidebar />
+<main style={{ flex: 1, padding: 24 }}>
+<Component {...pageProps} />
+</main>
+</div>
+)
+}
 
 const toggleSidebar = () => {
 setSidebarCollapsed(prev => {
