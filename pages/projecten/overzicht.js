0e8eb58313ae6507c0d8bb96c710f8a6467a6010
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-17 13:06:08 +0100

    Create overzicht.js

---
+++ pages/projecten/overzicht.js
@@ -0,0 +1,17 @@
+import PageHeader from "../../components/PageHeader"
+
+export default function ProjectenOverzicht() {
+return (
+<>
+<PageHeader title="Projecten" submoduleKey="overzicht" />
+
+  <div className="card">
+    <div className="card-body">
+      Projecten overzicht inhoud
+    </div>
+  </div>
+</>
+
+
+)
+}
