6020f205c6b96ab2190fd49fb2ab825a07d19865
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-19 11:32:17 +0100

    Update index.js

--- pages/mail/index.js
+++ pages/mail/index.js
@@ -1,11 +1,27 @@
+import { getMailAccounts } from "../../lib/mail"
 
-import PageShell from "../../components/PageShell"
+export async function getServerSideProps() {
+  const accounts = await getMailAccounts()
+  return { props: { accounts } }
+}
 
-export default function Mail() {
+export default function MailLanding({ accounts }) {
   return (
-    <PageShell
-      title="Mail"
-      description="Overzichtspagina voor mail"
-    />
+    <div>
+      <h1>Mail</h1>
+
+      <div className="row">
+        {accounts.map(acc => (
+          <div key={acc.id} className="col-md-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="fw-bold">{acc.label}</div>
+                <div className="text-muted">{acc.address}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
