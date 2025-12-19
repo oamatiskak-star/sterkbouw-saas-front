dff8437e80c5e6019c3d9f58264c25ade86708c8
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-19 09:05:45 +0100

    Update ModulePage.js

--- components/ModulePage.js
+++ components/ModulePage.js
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react"
 import { fetchModule } from "../lib/api"
+import ModuleRenderer from "./ModuleRenderer"
 
 export default function ModulePage({ modulePath, title }) {
-  const [data, setData] = useState(null)
+  const [components, setComponents] = useState([])
 
   useEffect(() => {
-    fetchModule(modulePath).then(setData).catch(console.error)
+    fetchModule(modulePath)
+      .then(res => {
+        if (Array.isArray(res)) setComponents(res)
+      })
+      .catch(console.error)
   }, [modulePath])
 
   return (
     <div>
       <h1>{title}</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+
+      {components.map((component, index) => (
+        <ModuleRenderer key={index} component={component} />
+      ))}
     </div>
   )
 }
