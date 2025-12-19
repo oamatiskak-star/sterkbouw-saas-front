49132570ea45800aa9a7c8f1ec8d6f2ae414fa61
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-19 19:46:47 +0100

    Update index.js

--- pages/projecten/index.js
+++ pages/projecten/index.js
@@ -2,11 +2,13 @@ import { NAVIGATION } from "../../config/navigation"
 import ModulePage from "../../components/ModulePage"
 
 export default function ProjectenLanding() {
-  const module = {
-    key: "projecten",
-    label: "Projecten",
-    description: "Project overzicht"
-  }
+  return (
+    <div>
+      <h1 style={{ fontSize: 24, fontWeight: 700, marginBottom: 16 }}>
+        Projecten
+      </h1>
 
-  return <ModulePage module={module} />
+      <p>Kies links een onderdeel.</p>
+    </div>
+  )
 }
