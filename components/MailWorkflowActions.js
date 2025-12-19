1796ec43f83de2daa8dd41bc72108dc9844d7edd
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-19 12:37:48 +0100

    Create MailWorkflowActions.js

---
+++ components/MailWorkflowActions.js
@@ -0,0 +1,19 @@
+import MailWorkflowButton from "./MailWorkflowButton"
+
+export default function MailWorkflowActions({ userId, actions }) {
+  if (!actions || actions.length === 0) return null
+
+  return (
+    <div className="d-flex gap-2">
+      {actions.map(action => (
+        <MailWorkflowButton
+          key={action.workflow_key}
+          userId={userId}
+          workflowKey={action.workflow_key}
+          label={action.label}
+          onRun={action.onRun}
+        />
+      ))}
+    </div>
+  )
+}
