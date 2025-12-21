e395a36cf4f000ead96c44d40b6cfb186278bc07
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-21 02:19:43 +0100

    Update OptForm.jsx

--- components/calculatie/OptForm.jsx
+++ components/calculatie/OptForm.jsx
@@ -2,8 +2,15 @@ export default function OptForm({ options, setOptions }) {
   return (
     <>
       {Object.keys(options).map(k => (
-        <label key={k}>
-          <input type="checkbox" checked={options[k]} onChange={e => setOptions({...options,[k]:e.target.checked})}/>
+        <label key={k} style={{ display: "block", marginBottom: 8 }}>
+          <input
+            type="checkbox"
+            checked={options[k]}
+            onChange={e =>
+              setOptions({ ...options, [k]: e.target.checked })
+            }
+            style={{ marginRight: 6 }}
+          />
           {k}
         </label>
       ))}
