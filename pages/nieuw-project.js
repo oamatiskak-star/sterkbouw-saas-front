f14f725fadec1d6bf17fb7ad5fc17ff1056f5404
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-23 01:16:24 +0100

    Update nieuw-project.js

--- pages/nieuw-project.js
+++ pages/nieuw-project.js
@@ -1,30 +1,12 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
 export default function Page() {
-  return (
-    <div>
-      <h1>Nieuw Project</h1>
+  const router = useRouter()
 
-      <div className="row my-4">
-        <div className="col"><div className="card"><div className="card-body">KPI 1</div></div></div>
-        <div className="col"><div className="card"><div className="card-body">KPI 2</div></div></div>
-        <div className="col"><div className="card"><div className="card-body">KPI 3</div></div></div>
-        <div className="col"><div className="card"><div className="card-body">KPI 4</div></div></div>
-      </div>
+  useEffect(() => {
+    router.replace("/calculaties/nieuw")
+  }, [router])
 
-      <div className="mt-4">
-        
-    <a href="/nieuw-project/import" className="btn btn-primary me-2 mb-2">
-      Importeer bestanden
-    </a>
-  
-    <a href="/nieuw-project/structuur" className="btn btn-primary me-2 mb-2">
-      Maak structuur
-    </a>
-  
-    <a href="/nieuw-project/taken" className="btn btn-primary me-2 mb-2">
-      Open taken
-    </a>
-  
-      </div>
-    </div>
-  )
+  return null
 }
