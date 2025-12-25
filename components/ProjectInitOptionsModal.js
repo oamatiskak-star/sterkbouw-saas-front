bcec9612d9f658217374e139bb702597c78c1e3c
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-25 23:38:08 +0100

    Update ProjectInitOptionsModal.js

--- components/ProjectInitOptionsModal.js
+++ components/ProjectInitOptionsModal.js
@@ -1,11 +1,6 @@
 import { useState, useRef } from "react"
 import { useRouter } from "next/router"
-import { createClient } from "@supabase/supabase-js"
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-)
+import supabase from "@/lib/supabase"
 
 /* ======================
    HARDE UPLOAD ZONDER SDK
