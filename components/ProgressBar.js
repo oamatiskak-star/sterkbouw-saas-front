ca00e7e66832a3ac4af3ba07cb80539adb1b43be
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-29 19:12:07 +0100

    Create ProgressBar.js

---
+++ components/ProgressBar.js
@@ -0,0 +1,26 @@
+// components/ProgressBar.js
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+export default function ProgressBar() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = () => NProgress.start()
+    const handleComplete = () => NProgress.done()
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  }, [router])
+
+  return null
+}
