916b3a4c9679dd8470e7512a52efa258f3fb625e
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-29 16:23:17 +0100

    Update index.js

--- components/ui/index.js
+++ components/ui/index.js
@@ -1,4 +1,13 @@
 // components/ui/index.js
 export { default as Button } from './button';
+export { default as Card } from './card';
 export { default as Input } from './input';
-export * from './card';
+export { default as Label } from './label';
+export { default as Textarea } from './textarea';
+export { default as Select } from './select';
+export { default as Badge } from './badge';
+export { default as Progress } from './progress';
+export { default as Separator } from './separator';
+export { default as Alert } from './alert';
+export * from './table';
+export * from './tabs';
