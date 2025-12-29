133eb2fdbba910916ba8f54ea00333c0b43a4962
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-29 19:53:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,10 +5,17 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  // Als je App Router gebruikt:
-  // experimental: {
-  //   appDir: true,
-  // },
+  experimental: {
+    optimizeCss: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // Zorg dat Tabler goed wordt opgelost
+      '@tabler/core': require.resolve('@tabler/core'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
