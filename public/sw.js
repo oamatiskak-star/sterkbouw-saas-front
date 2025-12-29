ec62108d672beabb6a88d42c7cca1544ebcb7ea1
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-29 19:33:16 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+// Service Worker for PWA
+const CACHE_NAME = 'sterkbouw-v1'
+const urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/favicon.ico'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME]
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    )
+  )
+})
