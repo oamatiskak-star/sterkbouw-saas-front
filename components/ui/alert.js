badd81e1767df9cfe0af715bc4a30e4c5efb23bf
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-29 16:22:34 +0100

    Create alert.js

---
+++ components/ui/alert.js
@@ -0,0 +1,23 @@
+// components/ui/alert.js
+export default function Alert({ 
+  children, 
+  variant = 'default',
+  className = '',
+  ...props 
+}) {
+  const variants = {
+    default: 'bg-gray-50 text-gray-900 border-gray-200',
+    destructive: 'bg-red-50 text-red-900 border-red-200',
+    warning: 'bg-yellow-50 text-yellow-900 border-yellow-200',
+    success: 'bg-green-50 text-green-900 border-green-200'
+  };
+
+  return (
+    <div
+      className={`rounded-lg border p-4 ${variants[variant]} ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
