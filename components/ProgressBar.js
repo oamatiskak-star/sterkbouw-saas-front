42be662ed2557687ed5d1e10c28da98ac6d0814c
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-29 19:18:15 +0100

    Update ProgressBar.js

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,16 +1,24 @@
-// components/ProgressBar.js - ZONDER NPROGRESS
+// components/ProgressBar.js
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 export default function ProgressBar() {
   const router = useRouter()
   const [progress, setProgress] = useState(0)
+  const [show, setShow] = useState(false)
 
   useEffect(() => {
-    const handleStart = () => setProgress(30)
+    const handleStart = () => {
+      setShow(true)
+      setProgress(30)
+    }
+    
     const handleComplete = () => {
       setProgress(100)
-      setTimeout(() => setProgress(0), 300)
+      setTimeout(() => {
+        setShow(false)
+        setProgress(0)
+      }, 300)
     }
 
     router.events.on('routeChangeStart', handleStart)
@@ -24,14 +32,15 @@ export default function ProgressBar() {
     }
   }, [router])
 
-  if (progress === 0) return null
+  if (!show) return null
 
   return (
-    <div className="fixed top-0 left-0 w-full h-1 bg-gray-200 z-50">
+    <div className="fixed top-0 left-0 right-0 z-[9999]">
       <div 
-        className="h-full bg-blue-600 transition-all duration-300 ease-out"
+        className="h-1 bg-gradient-to-r from-blue-500 to-blue-600 transition-all duration-300 ease-out"
         style={{ width: `${progress}%` }}
       />
+      <div className="h-1 bg-gray-200"></div>
     </div>
   )
 }
