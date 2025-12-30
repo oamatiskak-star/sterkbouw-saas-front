dba7afaee48a35e62f6f5ffd8620d3dec83146e2
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-30 01:58:43 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,16 @@
-import { NextResponse } from 'next/server'
+// middleware.js
+// AUTH MIDDLEWARE VOLLEDIG UITGESCHAKELD
+// Reden: Supabase client-auth regelt login state
+// Middleware veroorzaakte infinite redirect / hangende login
+
+import { NextResponse } from "next/server"
 
 export function middleware() {
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|public/).*)",
+  ],
 }
