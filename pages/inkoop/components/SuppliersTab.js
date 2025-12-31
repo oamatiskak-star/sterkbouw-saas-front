07d9aa29aa734e648fc28e491d1864e298606208
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-31 02:09:03 +0100

    Create SuppliersTab.js

---
+++ pages/inkoop/components/SuppliersTab.js
@@ -0,0 +1,14 @@
+export default function SuppliersTab({ suppliers, supplierPerformance, orders }) {
+  return (
+    <div>
+      <h5 className="card-title mb-4">
+        <i className="ti ti-truck text-primary me-2"></i>
+        Leveranciers Performance
+      </h5>
+      <div className="alert alert-info">
+        <i className="ti ti-info-circle me-2"></i>
+        Leveranciers performance dashboard
+      </div>
+    </div>
+  )
+}
