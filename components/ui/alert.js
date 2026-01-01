f121345a2747059c228edf0752b5a51b030153cc
oamatiskak-star <o.amatiskak@gmail.com>
2026-01-01 17:43:27 +0100

    Update alert.js

--- components/ui/alert.js
+++ components/ui/alert.js
@@ -1,5 +1,6 @@
 // components/ui/alert.js
-export default function Alert({ 
+
+export function Alert({ 
   children, 
   variant = 'default',
   className = '',
@@ -21,3 +22,22 @@ export default function Alert({
     </div>
   );
 }
+
+export function AlertTitle({ children, className = '', ...props }) {
+  return (
+    <h5 className={`font-semibold leading-none tracking-tight mb-2 ${className}`} {...props}>
+      {children}
+    </h5>
+  );
+}
+
+export function AlertDescription({ children, className = '', ...props }) {
+  return (
+    <div className={`text-sm ${className}`} {...props}>
+      {children}
+    </div>
+  );
+}
+
+// Optioneel: default export van het hoofd Alert component
+export default Alert;
