85252931d9c093dd8e2fdbc49bfcd215554c2bc6
oamatiskak-star <o.amatiskak@gmail.com>
2026-01-01 17:51:43 +0100

    Update index.js

--- components/ui/index.js
+++ components/ui/index.js
@@ -1,10 +1,14 @@
 // components/ui/index.js
-export { default as Alert } from './alert'
-export { default as Progress } from './progress'
-export { default as Card } from './card'
-export { default as Label } from './label'
-export { default as Select } from './select'
-export { default as Textarea } from './textarea'
-export { default as Badge } from './badge'
-export { default as Separator } from './separator'
-// ... alle andere componenten
+export { Alert, AlertTitle, AlertDescription } from './alert';
+export { Badge } from './badge';
+export { Button } from './button';
+export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './card';
+export { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription } from './dialog';
+export { Input } from './input';
+export { Label } from './label';
+export { Progress } from './progress';
+export { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from './select';
+export { Separator } from './separator';
+export { Table, TableHeader, TableBody, TableFooter, TableHead, TableRow, TableCell, TableCaption } from './table';
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './tabs';
+export { Textarea } from './textarea';
