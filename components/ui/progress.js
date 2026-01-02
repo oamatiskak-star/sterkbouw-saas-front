fe91314da8b55852c21a8f373674f8fb03aa255d
oamatiskak-star <o.amatiskak@gmail.com>
2026-01-02 14:32:03 +0100

    Update progress.js

--- components/ui/progress.js
+++ components/ui/progress.js
@@ -1,9 +1,9 @@
 // components/ui/progress.js
 export function Progress({ value = 0, className = '', ...props }) {
   return (
-    <div className={`h-2 w-full overflow-hidden rounded-full bg-secondary ${className}`} {...props}>
+    <div className={`h-2 w-full overflow-hidden rounded-full bg-gray-200 ${className}`} {...props}>
       <div
-        className="h-full w-full flex-1 bg-primary transition-all"
+        className="h-full w-full flex-1 bg-blue-600 transition-all"
         style={{ width: `${value}%` }}
       />
     </div>
