c6752535365d63c125e72ff28d38afdf8f5f9234
oamatiskak-star <o.amatiskak@gmail.com>
2026-01-10 15:21:12 +0100

    Update index.js

--- pages/calculaties/index.js
+++ pages/calculaties/index.js
@@ -2,9 +2,6 @@
 import { useState } from 'react';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-if (!API_BASE_URL) {
-  throw new Error('NEXT_PUBLIC_API_URL is not defined');
-}
 
 export default function CalculatiesPage() {
 
